4d210c831b3ca4b4a3f3e71c04376d6d3d44ffbf
luisaga215 <luis.aga215@gmail.com>
2018-12-13 20:13:04 -0600

    Border radius

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function homeDivs() {
-  $(".divRowHome").css("height", $(document).height());
+  let height = $(document).height();
+  $(".divRowHome").css("height", height);
+
+  let width = $(".divImgHome").width();
+  width = width + 30;
+  $(".divImgHome1").css("width", width);
+  $(".divImgHome2").css("width", width);
+  height = height - 80;
+  $(".divImgHome2").css("height", height);
 }
