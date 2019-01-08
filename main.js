ca274850c978bae8fb1950d06cb7b028dbd1290d
luisaga215 <luis.aga215@gmail.com>
2019-01-08 13:17:26 -0600

    Home done

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ function homeDivs() {
   height = height - 80;
   $(".divImgHome2").css("height", height);
 }
+
+function hoverFb() {
+  $(".divSocial #icon1 img").attr("src", "./assets/fb_blue.png");
+}
+
+function unhoverFb() {
+  $(".divSocial #icon1 img").attr("src", "./assets/fb_white.png");
+}
+
+function hoverTwitter() {
+  $("#icon2 img").attr("src", "./assets/twitter_2.png");
+}
+
+function unhoverTwitter() {
+  $("#icon2 img").attr("src", "./assets/twiter_icon.svg");
+}
